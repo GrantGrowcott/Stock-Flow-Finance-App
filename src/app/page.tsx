3e2370c45983231355dfecb7d15c15bc8b291c9b
Grant Growcott <grantgrowcott56@gmail.com>
2025-04-15 17:23:32 -0400

    Uncomment NewsWidget

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 "use client";
-// import NewsWidget  from "./components/NewsWidget";
+import NewsWidget  from "./components/NewsWidget";
 import TrendingStocks from "./components/TrendingStocks";
 
 function HomePage() {
   return (
     <div className="flex content-center justify-center p-7 gap-5 max-[900px]:flex-col" >
-      {/* <NewsWidget  /> */}
+      <NewsWidget  />
       <TrendingStocks />
     </div>
   );
