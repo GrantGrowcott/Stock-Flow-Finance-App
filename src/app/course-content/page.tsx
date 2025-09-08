a5d1204732a2f3791d54649d3bdeb42f288d6a26
Grant Growcott <grantgrowcott56@gmail.com>
2025-09-08 10:26:20 -0400

    Changes to the Secrets

--- src/app/course-content/page.tsx
+++ src/app/course-content/page.tsx
@@ -2,17 +2,17 @@ import { supabase } from "../../../lib/supabaseClient";
 import VideoGallery from "../components/VideoGallery";
 
 export default async function CourseContent() {
-  // List files from the "videos" bucket
+
   const { data: files, error } = await supabase.storage.from("videos").list();
 
   if (error) {
     console.error("Error fetching videos:", error.message);
   }
 
-  // Only keep actual video files
+
   const urls =
     files
-      ?.filter((file) => file.name.match(/\.(mp4|mkv|webm)$/i)) // adjust as needed
+      ?.filter((file) => file.name.match(/\.(mp4|mkv|webm)$/i)) 
       .map(
         (file) =>
           supabase.storage.from("videos").getPublicUrl(file.name).data.publicUrl
