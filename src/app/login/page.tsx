8b0ada2c732efce4d9778580cc78ba8f7e462c78
Grant Growcott <grantgrowcott56@gmail.com>
2025-09-08 15:54:57 -0400

    Changed Metadata

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,8 @@
 import LoginWidget from "../components/LoginWidget";
-
 export const metadata = {
   title: "Login",
   description: "Login Page for the Stock Flow Web Application.",
 };
-
 const Login = () => {
   return <LoginWidget />;
 };
