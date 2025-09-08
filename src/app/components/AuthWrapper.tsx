ceedbc83518c5f5d81c25da7474e72121c72e008
Grant Growcott <grantgrowcott56@gmail.com>
2025-09-08 16:14:37 -0400

    Some Changes to component logic

--- src/app/components/AuthWrapper.tsx
+++ src/app/components/AuthWrapper.tsx
@@ -6,18 +6,17 @@ import { setLoginState } from "../store/userSlice";
 import { supabase } from "../../../lib/supabaseClient";
 import Navbar from "./NavBar";
 import SearchBar from "./SearchBar";
-import Login from "../login/page";
-import PasswordRecovery from "../password-recovery/page";
-import PasswordReset from "../password-reset/page";
-import EmailRegistration from "../register/page";
-import { usePathname, useRouter } from "next/navigation";
+import LoginWidget from "./LoginWidget";
+import PasswordRecoveryWidget from "./PasswordRecoveryWidget";
+import PasswordResetWidget from "./PasswordResetWidget";
+import RegistrationWidget from "./RegistrationWidget";
+import { usePathname } from "next/navigation";
 
 export default function AuthWrapper({ children }: { children: React.ReactNode }) {
   const dispatch = useDispatch();
   const [collapsed, setCollapsed] = useState(false);
 
   const user = useSelector((state: RootState) => state.user.isLoggedIn);
-  const router = useRouter();
   const pathname = usePathname();
 
   const toggleNavbar = () => setCollapsed((prev) => !prev);
@@ -26,33 +25,28 @@ export default function AuthWrapper({ children }: { children: React.ReactNode })
     const checkSession = async () => {
       const { data } = await supabase.auth.getSession();
       if (data?.session) {
-        dispatch(setLoginState(true)); 
-        
+        dispatch(setLoginState(true));
       }
     };
 
     checkSession();
 
     const { data: authListener } = supabase.auth.onAuthStateChange((_event, session) => {
-      if (session) {
-        dispatch(setLoginState(true));
-      } else {
-        dispatch(setLoginState(false));
-      }
+      dispatch(setLoginState(!!session));
     });
 
     return () => {
       authListener?.subscription.unsubscribe();
     };
-  }, [dispatch, router]);
+  }, [dispatch]);
 
   const authPages = ["/register", "/password-recovery", "/password-reset", "/login"];
 
   if (!user) {
-    if (pathname === "/register") return <EmailRegistration />;
-    if (pathname === "/password-recovery") return <PasswordRecovery />;
-    if (pathname === "/password-reset") return <PasswordReset />;
-    return <Login />; 
+    if (pathname === "/register") return <RegistrationWidget />;
+    if (pathname === "/password-recovery") return <PasswordRecoveryWidget />;
+    if (pathname === "/password-reset") return <PasswordResetWidget />;
+    return <LoginWidget />;
   }
 
   return (
---
+++ src/app/components/PasswordResetWidget.tsx
@@ -0,0 +1,49 @@
+"use client";
+import Image from "next/image";
+import { useState } from "react";
+import { handlePassword } from "../../../helpers/helpers";
+import { useRouter } from "next/navigation";
+import { icons } from "@/constants";
+import Head from "next/head";
+
+const PasswordResetWidget = () => {
+  const [password, setPassword] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
+  const router = useRouter();
+
+  return (
+    <>
+      <Head>
+        <title>Password Reset</title>
+        <meta name="description" content="Page to recover the users password" />
+      </Head>
+      <div className="flex flex-col items-center justify-center h-screen bg-[url(/clouds.webp)] bg-cover">
+        <div className="bg-gradient-to-t from-[var(--white)] to-[var(--lightBlue)] p-7 mx-5 rounded-2xl flex flex-col">
+          <Image src="/login-icon.png" alt="Logo" width={icons.login} height={icons.login} className="mb-5 mx-auto" />
+          <h1 className="text-2xl font-bold text-center text-[var(--white)] drop-shadow-md">
+            Create your new password{" "}
+          </h1>
+          <div className="mt-4">
+            <input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="w-full p-2 mb-3 rounded-md"
+            />
+          </div>
+
+          <button
+            onClick={() => handlePassword(password, setSuccessMessage, router)}
+            className="bg-[var(--black)] text-[var(--white)] py-3 px-4 rounded-md my-4"
+          >
+            <h3>Reset</h3>
+          </button>
+          {successMessage && <p className="text-[var(--green)] text-center mt-4">{successMessage}</p>}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default PasswordResetWidget;
