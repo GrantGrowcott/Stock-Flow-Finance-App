cb18fe4a2a568f148899ba30e1f3cbe01bc17507
Grant Growcott <grantgrowcott56@gmail.com>
2025-09-08 15:39:37 -0400

    Change to Home Metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,17 @@
 "use client";
 import NewsWidget from "./components/NewsWidget";
-import Head from "next/head";
 // import TrendingStocks from "./components/TrendingStocks";
+
+
+export const metadata = {
+  title: "Stock Flow",
+  description:
+    "Home page of the Stock Flow App - track your portfolio, analyze stocks, and manage your finances with ease.",
+};
 
 function HomePage() {
   return (
     <>
-      <Head>
-        <title>Stock Flow</title>
-        <meta
-          name="description"
-          content="Home page of the Stock Flow App - track your portfolio, analyze stocks, and manage your finances with ease."
-        />
-      </Head>
       <div className="flex content-center justify-center p-7 gap-5 max-[900px]:flex-col">
         <NewsWidget />
         {/* This has been commented out of the home page because they only allow this through the paid API. I will consider uncommenting with paid customers when the time is right. */}
